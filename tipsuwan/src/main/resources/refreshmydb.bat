5e36993a06ce1d700714f29ed2247c86adfcb50c
Wijit Siripun <wijit.siripun@gmail.com>
2018-05-21 19:55:09 +0700

    Experiment with JDBCRealm (used for login asking ID/PW) with BASIC authentication (i.e., using chrome built-in login screen).

--- tipsuwan/src/main/resources/refreshmydb.bat
+++ tipsuwan/src/main/resources/refreshmydb.bat
@@ -32,3 +32,30 @@ CREATE TABLE Employees (
 insert into Employees (ACTIVE, PASSWORD, userRole, firstName, lastName) values (1, '123', 'FrontDesk', 'Beer', 'BeerLastName');
 insert into Employees (ACTIVE, PASSWORD, userRole, firstName, lastName) values (1, '123', 'Admin', 'B', 'BLastName');
 
+
+
+
+
+
+CREATE TABLE tomcat_users (
+	user_name varchar(20) NOT NULL PRIMARY KEY,
+	password varchar(32) NOT NULL
+);
+CREATE TABLE tomcat_roles (
+	role_name varchar(20) NOT NULL PRIMARY KEY
+);
+CREATE TABLE tomcat_users_roles (
+	user_name varchar(20) NOT NULL,
+	role_name varchar(20) NOT NULL,
+	PRIMARY KEY (user_name, role_name),
+	CONSTRAINT tomcat_users_roles_foreign_key_1 FOREIGN KEY (user_name) REFERENCES tomcat_users (user_name),
+	CONSTRAINT tomcat_users_roles_foreign_key_2 FOREIGN KEY (role_name) REFERENCES tomcat_roles (role_name)
+);
+INSERT INTO tomcat_users (user_name, password) VALUES ('deron', 'deronpass');
+INSERT INTO tomcat_users (user_name, password) VALUES ('larry', 'larrypass');
+INSERT INTO tomcat_roles (role_name) VALUES ('dude');
+INSERT INTO tomcat_roles (role_name) VALUES ('manager');
+INSERT INTO tomcat_users_roles (user_name, role_name) VALUES ('deron', 'dude');
+INSERT INTO tomcat_users_roles (user_name, role_name) VALUES ('deron', 'manager');
+INSERT INTO tomcat_users_roles (user_name, role_name) VALUES ('larry', 'manager');
+
--- tipsuwan/src/main/resources/tbd1.bat
+++ tipsuwan/src/main/resources/tbd1.bat
@@ -1,7 +1,12 @@
+show databases;
 USE db_tipsuwan;
 
 
 select * from Employees;
+
+select * from tomcat_users;
+select * from tomcat_roles;
+select * from tomcat_users_roles;
 
 
 
